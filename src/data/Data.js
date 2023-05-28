8de2c52fb7540af259571e8681f2d49ca6544a89
abhinav101901 <abhinav8299233838@gmail.com>
2023-05-28 13:32:24 +0530

    Quiz_App

---
+++ src/data/Data.js
@@ -0,0 +1,29 @@
+export const Quizdata=[
+    {
+    question:"Which of the following is not a bulit-in module in Node.js",
+    options:["fs","http","mysql","path",],
+    anwers:3
+    },
+    {
+        question:"_______ create a rough skecth of what the website will look like when completed.",
+        options:["WireFrames","Webflows","Wirefalls","Webframes",],
+        anwers:1
+        },
+
+        {
+            question:"Which CSS property allows you to set multiple list properties at once?",
+            options:["List-Style-Type","List-Style","List-Style-Position","List-Style-Image",],
+            anwers:2
+            },
+               {
+                question:"A 2D array having a different number of elements in each row is known as ______?",
+                options:["Spares","Danes","Regular","Irregular"],
+                anwers:4
+                },
+                {
+                    question:"In ASP.NET, each session uses a unique ______ bits identifier?",
+                    options:["60","180","120","30"],
+                    anwers:3
+                    },
+
+]
